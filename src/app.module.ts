637399a1c349b3231c875df933dc931a998afc98
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-07-07 13:47:41 +0700

    Add PrismaService and update module imports and controllers in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma.service';
+import { UsersService } from './users/users.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, AuthModule],
 })
 export class AppModule {}
