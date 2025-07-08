463018f7d257fb29a3343f48145e50627b23cb0e
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2025-07-08 16:52:22 +0000

    fix(cors): idk, but works :p

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // fix cors sementara :v
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3001);
