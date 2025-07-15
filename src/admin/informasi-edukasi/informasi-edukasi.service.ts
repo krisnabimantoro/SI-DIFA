a9e3a08e89c114ebabf3b87510a1bab9d02b5720
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-07-15 13:46:43 +0700

    feat(service): implement InformasiEdukasiService with CRUD operations

--- src/admin/informasi-edukasi/informasi-edukasi.service.ts
+++ src/admin/informasi-edukasi/informasi-edukasi.service.ts
@@ -1,4 +1,62 @@
 import { Injectable } from '@nestjs/common';
 
+import { Prisma, informasi_edukasi } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+
 @Injectable()
-export class InformasiEdukasiService {}
+export class InformasiEdukasiService {
+  constructor(private prisma: PrismaService) {}
+
+  async createInformasiEdukasi(
+    data: Prisma.informasi_edukasiCreateInput,
+  ): Promise<informasi_edukasi> {
+    return this.prisma.informasi_edukasi.create({
+      data,
+    });
+  }
+
+  async getInformasiEdukasi(
+    where: Prisma.informasi_edukasiWhereUniqueInput,
+  ): Promise<informasi_edukasi | null> {
+    return this.prisma.informasi_edukasi.findUnique({
+      where,
+    });
+  }
+
+  async updateInformasiEdukasi(
+    where: Prisma.informasi_edukasiWhereUniqueInput,
+    data: Prisma.informasi_edukasiUpdateInput,
+  ): Promise<informasi_edukasi> {
+    return this.prisma.informasi_edukasi.update({
+      where,
+      data,
+    });
+  }
+
+  async deleteInformasiEdukasi(
+    where: Prisma.informasi_edukasiWhereUniqueInput,
+  ): Promise<informasi_edukasi> {
+    return this.prisma.informasi_edukasi.delete({
+      where,
+    });
+  }
+
+  async getAllInformasiEdukasi(
+    params: {
+      skip?: number;
+      take?: number;
+      cursor?: Prisma.informasi_edukasiWhereUniqueInput;
+      where?: Prisma.informasi_edukasiWhereInput;
+      orderBy?: Prisma.informasi_edukasiOrderByWithRelationInput;
+    } = {},
+  ): Promise<informasi_edukasi[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.informasi_edukasi.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+}
