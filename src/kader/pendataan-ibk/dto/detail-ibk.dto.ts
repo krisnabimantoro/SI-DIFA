62de86e76e9a11bee1c77ca435c65b578e7256ab
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-07-22 02:44:07 +0700

    feat(detail-ibk): add DetailIbkDto class for detailed IBK data representation

---
+++ src/kader/pendataan-ibk/dto/detail-ibk.dto.ts
@@ -0,0 +1,13 @@
+export class DetailIbkDto {
+  readonly id?: string;
+  readonly pekerjaan?: string;
+  readonly pendidikan?: string;
+  readonly status_perkawinan?: string;
+  readonly titik_koordinat?: string;
+  readonly keterangan_tambahan?: string;
+  readonly jenis_dokumen?: string;
+  readonly file_dokumen?: string;
+  readonly created_at?: Date;
+  readonly updated_at?: Date;
+  readonly deleted_at?: Date;
+}
