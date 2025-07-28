77ee6c78de2b59d5b3dcfbbabce5be521ca61fb0
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-07-29 01:26:57 +0700

    fix(Dockerfile): switch base image to node:20-alpine for a smaller footprint fix(app.service): update getHello method response to include branding

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use the official Node.js image as the base image
-FROM node:20
+FROM node:20-alpine
 
 # Install pnpm globally
 RUN corepack enable && corepack prepare pnpm@latest --activate
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'API SI-DIFA copyright 2025';
+    return 'API SI-DIFA copyright 2025 - labitumm';
   }
 }
