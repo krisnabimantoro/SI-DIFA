f95c166b1e33e729c34e39852d7eba6fb8cba13f
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-08-04 15:16:12 +0700

    feat(kader): implement CreateJadwalPosyanduDto, update controller and service for jadwal posyandu

--- src/kader/jadwal-posyandu/jadwal-posyandu.controller.ts
+++ src/kader/jadwal-posyandu/jadwal-posyandu.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { JadwalPosyanduService } from './jadwal-posyandu.service';
 import { CreateJadwalPosyanduDto } from './dto/create-jadwal-posyandu.dto';
 import { UpdateJadwalPosyanduDto } from './dto/update-jadwal-posyandu.dto';
+import { Roles } from 'src/decorator/roles.decorator';
+import { JwtAuthGuard } from 'src/guards/jwt-auth-guard';
+import { RolesGuard } from 'src/guards/roles.guard';
 
-@Controller('jadwal-posyandu')
+@Controller('/kader/jadwal-posyandu')
 export class JadwalPosyanduController {
   constructor(private readonly jadwalPosyanduService: JadwalPosyanduService) {}
 
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post()
-  create(@Body() createJadwalPosyanduDto: CreateJadwalPosyanduDto) {
-    return this.jadwalPosyanduService.create(createJadwalPosyanduDto);
+  @Roles('kader')
+  async createLowongan(@Body() data: CreateJadwalPosyanduDto): Promise<any> {
+    return this.jadwalPosyanduService.create(data.posyandu_id, data);
   }
 
   @Get()
@@ -23,7 +40,10 @@ export class JadwalPosyanduController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateJadwalPosyanduDto: UpdateJadwalPosyanduDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateJadwalPosyanduDto: UpdateJadwalPosyanduDto,
+  ) {
     return this.jadwalPosyanduService.update(+id, updateJadwalPosyanduDto);
   }
 
--- src/kader/jadwal-posyandu/jadwal-posyandu.service.ts
+++ src/kader/jadwal-posyandu/jadwal-posyandu.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateJadwalPosyanduDto } from './dto/create-jadwal-posyandu.dto';
 import { UpdateJadwalPosyanduDto } from './dto/update-jadwal-posyandu.dto';
+import { PrismaService } from 'src/prisma.service';
+
+import { Prisma, jadwal_posyandu, posyandu } from '@prisma/client';
 
 @Injectable()
 export class JadwalPosyanduService {
-  create(createJadwalPosyanduDto: CreateJadwalPosyanduDto) {
-    return 'This action adds a new jadwalPosyandu';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(
+    posyanduId: string,
+    createJadwalPosyanduDto: CreateJadwalPosyanduDto,
+  ): Promise<jadwal_posyandu> {
+    // Remove posyandu_id from DTO to avoid conflict with relation
+    const { posyandu_id, ...dataJadwalPosyandu } = createJadwalPosyanduDto;
+    return this.prismaService.jadwal_posyandu.create({
+      data: {
+        posyandu: {
+          connect: { id: posyanduId },
+        },
+        ...dataJadwalPosyandu,
+        created_at: new Date(),
+      },
+    });
   }
 
   findAll() {
